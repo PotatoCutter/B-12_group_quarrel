80d8c60fc717b964ad3152d3276f2e4efda90326
brighto <jychoi1996@gmail.com>
2023-05-09 19:50:43 +0900

    add users_view put_delete

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,6 @@ from users import views
 
 urlpatterns = [
     path('signup/', views.SignupView.as_view()),
+    path('', views.UserView.as_view()),
     path('<int:user_id>/', views.UserView.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -3,7 +3,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import permissions
-from .serializer import UserSerializer
+from rest_framework.generics import get_object_or_404
+from .serializers import UserSerializer
+from .models import User
 
 class SignupView(APIView):
     def post(self, request):
@@ -16,12 +18,35 @@ class SignupView(APIView):
         return Response({"message":"signup ok"},status=status.HTTP_201_CREATED)
     
 class UserView(APIView):
-    def get(self, request):
+    def get(self, request, user_id=None):
         '''유저 조회'''
-        pass
-    def put(self, request):
+        return Response({"message":"유저 조회"})
+    
+    def put(self, request, user_id=None):
         '''유저 수정'''
+        # 나중에 수정 해야함. get_or_404
+        if str(request.user) == "AnonymousUser":
+            return Response({"message":"익명 유저"}, status=status.HTTP_403_FORBIDDEN)
+        # end line
+        
+        # id 있을때
+        if user_id:
+            user = User.objects.filter(id= user_id)
+            serial = UserSerializer(user, data=request.data)
+            if serial.is_valid(raise_exception=True):
+                serial.save()
+                return Response(serial.data, status=status.HTTP_200_OK)
+        # id 없으면
+        user = UserSerializer(request.user, data = request.data) # 검토가 필요 arg 1 
+        user.is_valid(raise_exception=True)
+        user.save()
+        return Response({"message":"유저 정보 수정"}, status=status.HTTP_200_OK)
         pass
-    def delete(self, request):
+    
+    def delete(self, request, user_id=None):
         '''유저 삭제'''
-        pass
+        user =  get_object_or_404(User, user_id)
+        if request.user.id == user:
+            user.delete(id= user_id )
+            return Response({"message":"유저 삭제"}, status=status.HTTP_200_OK)
+            
