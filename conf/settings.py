e8d2293cb5b45c8fc1dd8be9ab295783315f9fda
brighto <jychoi1996@gmail.com>
2023-05-10 18:04:13 +0900

    add send_email func

--- conf/settings.py
+++ conf/settings.py
@@ -206,3 +206,18 @@ SIMPLE_JWT = {
 }
 
 AUTH_USER_MODEL = "users.User"
+
+# 이메일링 서비스 세팅
+
+#smtp 세팅 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'   # 이메일 전송을 위한 백엔드
+EMAIL_HOST = "smtp.gmail.com"   # 이메일 호스트 주소
+EMAIL_PORT = 587                # 이메일 포트번호
+
+# 이메일 계정 세팅
+EMAIL_HOST_USER = 'jychoi1996@gmail.com'    # 보내는 이메일 계정
+EMAIL_HOST_PASSWORD = 'fsrunjsgaeucpfof'    # 보내는 이메일 계정의 호스트 패스워드
+EMAIL_USE_TLS = True                        # 서버 전송을 위한 보안설정 587 포트에서 사용됨
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER        # 기본 이메일 호스트 계정 설정
+
+# fsrunjsgaeucpfof
