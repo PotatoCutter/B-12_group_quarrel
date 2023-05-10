b6930582a088131e11efd842405086a78cad4de8
BenCyera <dltpgml1124@gmail.com>
2023-05-10 21:38:38 +0900

    views.py_ArticleLikes,CommentLikes,ArticleUserView/urls.py추가/models.py_likes추가

--- articles/admin.py
+++ articles/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from articles.models import Categorys, Article, Comment
+from articles.models import Categorys, Article, Comment, ArticleLikes, CommentLikes
 
 
 admin.site.register(Categorys)
 admin.site.register(Article)
 admin.site.register(Comment)
+admin.site.register(ArticleLikes)
+admin.site.register(CommentLikes)
