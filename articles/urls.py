50b76785db5afd83ead6f1475f881a364b950066
BenCyera <dltpgml1124@gmail.com>
2023-05-12 09:30:50 +0900

    Like clear/Like count,bookmark count

--- articles/urls.py
+++ articles/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from articles import views
 
 urlpatterns = [
-    path('<int:category_id>/',views.ArticleListView.as_view(), name = 'article_detail_view'),
+    path('<int:category_id>/',views.ArticleListView.as_view(), name = 'article_view'),
+    path('<int:category_id>/best/',views.ArticleBestListView.as_view(), name = 'article_best_view'),
     path('detail/<int:article_id>/', views.ArticleDetailView.as_view(), name='article_detail_view'),
     path('detail/<int:article_id>/comment/', views.CommentView.as_view(), name='comment_view'),
     path('detail/<int:article_id>/comment/<int:comment_id>/', views.CommentDetailView.as_view(), name='comment_detail_view'),
-    path('detail/<int:article_id>/likes/', views.ArticleLikes.as_view(), name='article_likes'),
-    path('detail/<int:article_id>/comment/<int:comment_id>/likes/', views.CommentLikes.as_view(), name='comment_likes'),
+    path('detail/<int:article_id>/likes/', views.ArticleLikesView.as_view(), name='article_likes_view'),
+    path('detail/<int:article_id>/comment/<int:comment_id>/likes/', views.CommentLikesView.as_view(), name='comment_likes_view'),
     path('<int:user_id>/user_article/',views.ArticleUserView.as_view(), name = 'article_user_view'),
     path('detail/<int:article_id>/bookmark/', views.BookMarkView.as_view(), name='bookmark_view'),
     path('bookmark/<int:user_id>/', views.BookMarkView.as_view(), name='bookmark_view'),
